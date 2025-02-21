5e2804f210d391c17758414b9a4e7d6557c813cb
Adair-Gmz <martinezgomezadair@gmail.com>
2025-02-21 17:56:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Función para cambiar el color del texto
 function cambiarColor() {
     var colors = ['red', 'green', 'blue'];
-    document.getElementById("change").style.color = colors[Math.floor(Math.random() * colors.length)];;
-    //document.getElementById("change").style.color = "blue";
+    document.getElementById("change").style.color = colors[Math.floor(Math.random() * colors.length)];
 }
