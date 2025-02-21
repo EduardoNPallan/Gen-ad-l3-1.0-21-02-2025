7931654d8c1aa909379efc50715b59287e7f8c8f
Adair-Gmz <martinezgomezadair@gmail.com>
2025-02-21 17:49:30 -0600

    Retoques CSS y JS

---
+++ script.js
@@ -0,0 +1,6 @@
+// Función para cambiar el color del texto
+function cambiarColor() {
+    var colors = ['red', 'green', 'blue'];
+    document.getElementById("change").style.color = colors[Math.floor(Math.random() * colors.length)];;
+    //document.getElementById("change").style.color = "blue";
+}
